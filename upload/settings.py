f36f1a8f5f4e9c4ed0c787248611314341df1a1e
iamUmairMughal <61555518+iamUmairMughal@users.noreply.github.com>
2021-02-08 13:52:54 +0500

    Update settings.py

--- upload/settings.py
+++ upload/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^p&%e#hvd8$#sbi0sn62gwn*^zglij^#&qex(yv*7om3ku#jve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
